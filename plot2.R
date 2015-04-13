cd9e3bee6ea0a359b69a3c17a30cae5384189e71
Tzu-En Ngiao <tengiao@gmail.com>
2015-04-13 23:07:10 +1200

    Commit 13.04.2015

--- plot2.R
+++ plot2.R
@@ -1,11 +1,12 @@
 file <- read.csv("household_power_consumption.txt", sep = ";", colClasses = "character")
-Date <- as.Date(file[, "Date"], format = "%d/%m/%Y")
-#Time <- strptime(file[, "Time"], format = "%H:%M:%S")
+Date <- as.Date(file[, "Date"], format = "%d/%m/%Y") #Format to %Y-%m-%d
 Time <- file[, "Time"]
+DateTime <- paste(Date, Time)
+TimeStamp <- strptime(DateTime, format = "%Y-%m-%d %H:%M:%S")
 GlobalActivePower <- as.numeric(file[, "Global_active_power"])
-newtable <- data.frame(Date, Time, GlobalActivePower)
-active.power.data <- subset(newtable, Date >= "2007-02-01" & Date <= "2007-02-02", select = c(Time, GlobalActivePower))
-with(active.power.data, plot(Time, GlobalActivePower, main = "", xaxt = "n", xlab = "", ylab = "Global Active Power (kilowatts)"))
+newtable <- data.frame(Date, TimeStamp, GlobalActivePower)
+newtable.sub <- subset(newtable, Date >= "2007-02-01" & Date <= "2007-02-02", select = c(TimeStamp, GlobalActivePower))
+with(newtable.sub, plot(TimeStamp, GlobalActivePower, main = "", pch = ".", type = "o", xlab = "", ylab = "Global Active Power (kilowatts)"))
 dev.copy(png, file = "plot2.png")
 dev.off()
 
